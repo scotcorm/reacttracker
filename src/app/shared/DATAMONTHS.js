910d08c634640ce5a7953dd31129ce5604c85e0c
Scott Cormier <chicormier@gmail.com>
2024-02-18 12:07:49 -0600

    initial commit

---
+++ src/app/shared/DATAMONTHS.js
@@ -0,0 +1,103 @@
+// import reactLakeImg from '../assets/img/react-lake.jpg';
+// import chromeRiverImg from '../assets/img/chrome-river.jpg';
+// import breadCrumbImg from '../assets/img/breadcrumb-trail.jpg';
+// import reduxWoodsImg from '../assets/img/redux-woods.jpg';
+
+export const DATAMONTHS = [
+  {
+    id: 0,
+    year: '2023',
+    label: 'January',
+    completed: '35',
+    cohort: '1097',
+    overlaps: '20',
+  },
+  {
+    id: 1,
+    year: '2023',
+    label: 'February',
+    completed: '37',
+    cohort: '1110',
+    overlaps: '22',
+  },
+  {
+    id: 2,
+    year: '2023',
+    label: 'March',
+    completed: '37',
+    cohort: '1188',
+    overlaps: '20',
+  },
+  {
+    id: 3,
+    year: '2023',
+    label: 'April',
+    completed: '36',
+    cohort: '1222',
+    overlaps: '21',
+  },
+  {
+    id: 4,
+    year: '2023',
+    label: 'May',
+    completed: '42',
+    cohort: '1240',
+    overlaps: '32',
+  },
+  {
+    id: 5,
+    year: '2023',
+    label: 'June',
+    completed: '43',
+    cohort: '1218',
+    overlaps: '42',
+  },
+  {
+    id: 6,
+    year: '2023',
+    label: 'July',
+    completed: '39',
+    cohort: '1232',
+    overlaps: '33',
+  },
+  {
+    id: 7,
+    ' year': '2023',
+    label: 'August',
+    completed: '45',
+    cohort: ' 1224',
+    overlaps: '30',
+  },
+  {
+    id: 8,
+    year: '2023',
+    label: 'September',
+    completed: '45',
+    cohort: '1226',
+    overlaps: '44',
+  },
+  {
+    id: 9,
+    year: '2023',
+    label: 'October',
+    completed: '41',
+    cohort: ' 1252',
+    overlaps: '34',
+  },
+  {
+    id: 10,
+    year: '2023',
+    label: 'November',
+    completed: '43',
+    cohort: '1198',
+    overlaps: '18',
+  },
+  {
+    id: 11,
+    year: '2023',
+    label: 'December',
+    completed: '38',
+    cohort: '1177',
+    overlaps: '29',
+  },
+];
